0b001b2aa712bf158c9f753c6e2328d1c056baee
Shriyashzzz <ghimires1@my.smccd.edu>
2026-03-18 15:32:35 -0700

    add style & interative buttons w the help of js.

--- js.js
+++ js.js
@@ -1,5 +1,3 @@
-
-
 function getComputerChoice(){
     let randomNum = getRandomInt(0,3) //IF MINUMUM ==1, min = 0 | IF MAXIMUM ==3, max =3
 
@@ -19,7 +17,7 @@ function getRandomInt(min, max){
 }
 
 
-function getHumanChoice(){
+/* function getHumanChoice(){
 
     let humanChoice = prompt("CHOOSE: ROCK, PAPER OR SCISSOR?")
     
@@ -32,7 +30,7 @@ function getHumanChoice(){
     return humanChoice.toUpperCase()
     
 
-    /*better way to do this , removes the risk of Stack OverFlow
+    better way to do this , removes the risk of Stack OverFlow
     let humanChoice = "";
 
     Keep looping as long as the choice is NOT valid
@@ -48,59 +46,138 @@ function getHumanChoice(){
     */
 
 
-}
     
 
-function playRound (computerChoice =getComputerChoice(), humanChoice = getHumanChoice()){
+function playRound (humanChoice, computerChoice =getComputerChoice()){
 
-    
+    let resultVal = [] // index 0: (0:draw, 1: win, -1: lose) index 1: string vlaue with computer and user chouce. index 2: computerChocie 
     if(humanChoice == computerChoice){
-        alert("DRAW, You both chose: " + humanChoice)
-        return false
+        resultVal[0] = 0;
+        resultVal[1]= `DRAW, You both chose: ${humanChoice}`;
+        resultVal[2] = computerChoice;
 
-    }else if(humanChoice == "PAPER" && computerChoice =="Rock" )
-        {
-         alert("You win with " + humanChoice)
-         alert("Computer Choice : " + computerChoice, "Human Choice: " + humanChoice)
-         return true
+        return resultVal;
 
+    }else if(humanChoice == "PAPER" && computerChoice =="Rock" ) {
+        resultVal[0] = 1;
+        resultVal[1] = `Computer Choice: ${computerChoice}, Human Choice:  ${humanChoice}`;
+        resultVal[2] = computerChoice;
+        return resultVal;
     }else if (humanChoice == "SCISSOR" && computerChoice == "PAPER"  ){
-        promt("You win with " + humanChoice)
-        alert("Computer Choice : " + computerChoice, "Human Choice: " + humanChoice)
-        return true
+        resultVal[0] = 1 ;
+        resultVal[1] `Computer Choice: ${computerChoice}, Human Choice: ${humanChoice}`;
+        resultVal[2] = computerChoice;
+
+        return resultVal;
     }else if(humanChoice == "ROCK" && computerChoice == "SCISSOR"  ){
-        alert("You win with " + humanChoice)
-        alert("Computer Choice : " + computerChoice, "Human Choice: " + humanChoice)
-        return true
+        
+        resultVal[0] = 1 ;
+        resultVal[1] = `Computer Choice : " + ${computerChoice}, Human Choice: ${humanChoice}`;
+        resultVal[2] = computerChoice;
+
+        return resultVal;
     }else if(humanChoice == "PAPER" && computerChoice == "ROCK"  ){
-        alert("You win with " + humanChoice)
-        alert("Computer Choice : " + computerChoice, "Human Choice: " + humanChoice)
-        return true
+
+        resultVal[0] = 1;
+        resultVal[1] = `Computer Choice :${computerChoice}, Human Choice: ${humanChoice}`;
+        resultVal[2] = computerChoice;
+
+        return resultVal;
     }
     else{
-        alert("You lose with " + humanChoice)
-        alert("Computer Choice : " + computerChoice, "Human Choice: " + humanChoice)
-        return false
+        resultVal[0] = -1;
+        resultVal[1] = `Computer Choice : ${computerChoice}, "Human Choice: ${humanChoice}`;
+        resultVal[2] = computerChoice;
+
+        return resultVal;
     }
     
 }
 
 
-function startGame(){
-    let gameCount = 5;
-    alert(`You have ${gameCount } tries to win against the clanker!`)
-    let didUserWin = false;
-    while(gameCount >0 && didUserWin == false ){
-        didUserWin = playRound();
+const rockBtn = document.createElement('button');
+const paperBtn = document.createElement('button');
+const scissorBtn = document.createElement('button');
+
+rockBtn.textContent = "Rock";
+paperBtn.textContent = "Paper";
+scissorBtn.textContent="Scissors";
+
+rockBtn.classList.add("gameButton");
+paperBtn.classList.add("gameButton");
+scissorBtn.classList.add("gameButton");
+
+const buttonSpan = document.createElement('span');
+buttonSpan.append(rockBtn, paperBtn, scissorBtn);
+
+buttonSpan.classList.add("buttonDiv");
+const gameDiv = document.querySelector("#gameContainer");
+gameDiv.appendChild(buttonSpan)
+
+
+const resultPara = document.createElement('p')
+resultPara.classList.add("resultPara")
+let computerScore = 0;
+let humanScore = 0;
+
+resultDiv = document.createElement('div');
+resultDiv.appendChild(resultPara);
+
+gameDiv.appendChild(resultDiv);
+
+
+const winnerPara = document.createElement('p');
+gameDiv.append(winnerPara);
+winnerPara.classList.add("winnerPara", "resultPara")
+
+
+const clankerChoicePara = document.createElement('p');
+clankerChoicePara.id  = "clankerId";
+
+buttonSpan.before(clankerChoicePara);
+
+buttonSpan.addEventListener('click', function(e){
+    if(e.target == rockBtn || e.target == paperBtn || e.target == scissorBtn){
+        const humanChoice = e.target.textContent.toUpperCase();
+        const result = playRound(humanChoice)
+        console.log(`Result Array From playRound Function: ${result}`)
+        clankerChoicePara.textContent = `Calnker Chose: ${result[2]}`
+        appendScore(result);
+        console.log(`Clanker: ${computerScore}, You: ${humanScore}`);
+        console.log(`clanker choice = ${result[2]}`)
+        resultPara.textContent = `Clanker: ${computerScore}  You: ${humanScore}`
     }
+})
+
+function appendScore(givenresult){
+    if (givenresult[0]  == 1){
+        humanScore +=1
+        
+    }else if (givenresult[0] == -1){
+        computerScore += 1
+
+    }
+    checkIfWon();
 }
 
+function checkIfWon(){
+    if (humanScore == 5){
+        clankerChoicePara.textContent = "";
+        winnerPara.textContent = "You have saved the humanity! You can officially call yourself Clanker beater."
+        winnerPara.textContent= winnerPara.textContent.toUpperCase()
 
-const start_btn = document.querySelector("#startGameBtn");
+        document.querySelectorAll('.gameButton').forEach(btn => btn.disabled = true);
+        
+    }else if(computerScore ==5){
+        clankerChoicePara.textContent = "";
+        winnerPara.textContent = "You are a disgrace to humanity, Clankers are taking over now. beep beep boop boop"
+        winnerPara.textContent = winnerPara.textContent.toUpperCase()
+        document.querySelectorAll('.gameButton').forEach(btn => btn.disabled = true);
+        
 
 
-start_btn.textContent = ("Start Game!");
-start_btn.style.color = ("green");
+    }
+    
+    
+}
 
-
-start_btn.addEventListener("click", startGame) 
